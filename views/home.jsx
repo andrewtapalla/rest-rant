b4864c8e3d686b558f4875ea849a43aca9440b75
Andrew Tapalla <tapalla_andrew@yahoo.com>
2022-01-15 11:36:55 -0800

    Adding styles to REST-rant

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home() {
         <Def>
             <main>
                 <h1>HOME</h1>
+                <div>
+                    <img src="/rest-rant/public/images/steak.jpg" alt="mouth watering steak" />
+                    <div>
+                        Photo by <a href="https://unsplash.com/@alexmunsell?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Alex Munsell</a> on <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                    </div>
+                </div>
                 <a href="/places">
                 <button className="btn-primary">Places Page</button>
                 </a>
