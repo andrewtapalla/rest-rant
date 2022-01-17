7eb0b65866c9ffebf66926e26d7cc9142890aa9c
Andrew Tapalla <tapalla_andrew@yahoo.com>
2022-01-17 11:15:58 -0800

    Adding bootstrap grid to index page

--- views/home.jsx
+++ views/home.jsx
@@ -7,7 +7,7 @@ function home() {
             <main>
                 <h1>HOME</h1>
                 <div>
-                    <img src="/rest-rant/public/images/steak.jpg" alt="mouth watering steak" />
+                    <img src="/images/steak.jpg" width="450" height="450" />
                     <div>
                         Photo by <a href="https://unsplash.com/@alexmunsell?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Alex Munsell</a> on <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
                     </div>
