8e439d3b57028d73adad0860b5d53f06d395c285
Andrew Tapalla <tapalla_andrew@yahoo.com>
2022-02-01 10:53:43 -0800

    Added commentary

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require('../models')
 
+// INDEX
 router.get('/', (req, res) => {
     db.Place.find()
     .then((places) => {
@@ -33,17 +34,43 @@ router.post('/', (req, res) => {
   })
 })
 
+// ADD A PLACE
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+// SHOW PAGE
 router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
+  .populate('comments')
   .then(place => {
+      console.log(place.comments)
       res.render('places/show', { place })
   })
   .catch(err => {
       console.log('err', err)
+      res.render('error404')
+  })
+})
+
+// COMMENTS
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
       res.render('error404')
   })
 })
@@ -69,98 +96,3 @@ router.delete('/:id/rant/:rantId', (req, res) => {
 })
 
 module.exports = router
-
-
-
-
-
-
-
-
-
-
-
-// const router = require('express').Router()
-// const places = require('../models/places.js')
-
-// router.get('/', (req, res) => {
-//     res.render('places/index', {places})
-// })
-
-// // NEW
-// router.get('/new', (req, res) => {
-//   res.render('places/new')
-// })
-
-// // UPDATE
-// router.put('/:id', (req, res) => {
-//   let id = Number(req.params.id)
-//   if (isNaN(id)) {
-//       res.render('error404')
-//   }
-//   else if (!places[id]) {
-//       res.render('error404')
-//   }
-//   else {
-//       if (!req.body.pic) {
-//           req.body.pic = 'http://placekitten.com/400/400'
-//       }
-//       if (!req.body.city) {
-//           req.body.city = 'Anytown'
-//       }
-//       if (!req.body.state) {
-//           req.body.state = 'USA'
-//       }
-//       places[id] = req.body
-//       res.redirect(`/places/${id}`)
-//   }
-// })
-
-// // SHOW
-// router.get('/:id', (req, res) => {
-//   let id = Number(req.params.id)
-//   if (isNaN(id)) {
-//     res.render('error404')
-//   }
-//   else if (!places[id]) {
-//     res.render('error404')
-//   }
-//   else {
-//     res.render('places/show', { place: places[id], id })
-//   }
-// })
-
-// // EDIT
-// router.get('/:id/edit', (req, res) => {
-//   let id = Number(req.params.id)
-//   if (isNaN(id)) {
-//       res.render('error404')
-//   }
-//   else if (!places[id]) {
-//       res.render('error404')
-//   }
-//   else {
-//     res.render('places/edit', { place: places[id], id })
-//   }
-// })
-
-
-
-// // DELETE
-// router.delete('/:id', (req, res) => {
-//   let id = Number(req.params.id)
-//   if (isNaN(id)) {
-//     res.render('error404')
-//   }
-//   else if (!places[id]) {
-//     res.render('error404')
-//   }
-//   else {
-//     places.splice(id, 1)
-//     res.redirect('/places')
-//   }
-// })
-
-
-
-// module.exports = router
